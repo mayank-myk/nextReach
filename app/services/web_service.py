92b4894291cc9fc4112671a6be5474d24c2a7373
mayank <mayank.agrawal@phonepe.com>
2025-01-11 11:03:39 +0530

    fourth commit

--- app/services/web_service.py
+++ app/services/web_service.py
@@ -1,6 +1,9 @@
 import concurrent.futures
+import json
+import random
 
 import instaloader
+import requests
 
 from app.api_requests.calculate_earning_request import CalculateEarningRequest
 from app.api_requests.waitlist_request import WaitListRequest
@@ -81,7 +84,7 @@ def calculate_influencer_earning(request: CalculateEarningRequest) -> str:
     return str(int(content_price + earnings_per_post)) + " per post"
 
 
-def calculate_engagement_rate(username: str) -> EnagementMetric | GenericResponse:
+def calculate_engagement_rate2(username: str) -> EnagementMetric | GenericResponse:
     if username == "success":
         return EnagementMetric(
             engagement_rate=3.5,
@@ -96,12 +99,44 @@ def calculate_engagement_rate(username: str) -> EnagementMetric | GenericRespons
                                button_text="Retry", header="Error")
 
 
-def calculate_engagement_rate1(username: str) -> EnagementMetric | GenericResponse:
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 9; Pixel 3 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 13_6_1 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; Galaxy S10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Mobile Safari/537.36"
+]
+
+# More realistic headers with dynamic and rotating headers
+HEADERS = {
+    "User-Agent": random.choice(USER_AGENTS),  # Randomly select a User-Agent from the list
+    "Accept-Language": "en-US,en;q=0.9,ru;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.instagram.com/",
+    "Accept": "*/*"
+    # "x-ig-app-id": "936619743392459",  # Instagram app id (static for all)
+    # "x-ig-www-claim": "hmac.4e2d882f1c0ac7d2c6c1de1b318f69b7ff84d052",  # Claim header (static for all)
+    # "x-ig-session-id": str(random.randint(100000000, 999999999)),  # Random session ID
+    # "x-ig-android-id": str(random.randint(1000000000000000, 9999999999999999)),  # Random Android ID
+    # "x-ig-android-serial": str(random.randint(1000000000000000, 9999999999999999)),  # Random Android serial
+    # "x-ig-device-id": str(random.randint(1000000000000000, 9999999999999999)),  # Device ID
+    # "x-ig-ds-user-id": str(random.randint(1000000000, 9999999999)),  # User ID (randomized)
+    # "x-ig-connection-type": "WIFI",  # Simulate WIFI connection
+    # "x-ig-batch": "1",  # Batch request type
+}
+
+
+def calculate_engagement_rate(username: str) -> EnagementMetric | GenericResponse:
     L = instaloader.Instaloader()
 
     try:
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = executor.submit(load_profile_data, L, username)
+            future = executor.submit(scrap_data_using_official_api, username)
             result = future.result(timeout=30)
             return result
 
@@ -121,7 +156,124 @@ def calculate_engagement_rate1(username: str) -> EnagementMetric | GenericRespon
         )
 
 
-def load_profile_data(L, username: str) -> EnagementMetric | GenericResponse:
+def get_instagram_data(username: str):
+    # Instagram API URL
+    url = f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}"
+
+    # Make the GET request to fetch profile data
+    response = requests.get(url, headers=HEADERS)
+
+    if response.status_code != 200:
+        return None
+
+    try:
+        profile_data = response.json()
+    except UnicodeError:
+        profile_data = json.loads(response.text)
+    except json.JSONDecodeError:
+        return None
+
+    return profile_data
+
+
+def scrap_data_using_official_api(username: str) -> EnagementMetric | GenericResponse:
+    profile_data = get_instagram_data(username)
+
+    if not profile_data:
+        return GenericResponse(
+            success=False,
+            message="We encountered an issue while fetching data from Instagram. Please try again later.",
+            button_text="Retry",
+            header="Oops"
+        )
+
+    # Extract the user data from the profile JSON response
+    user_data = profile_data.get("data", {}).get("user", {})
+
+    if not user_data:
+        return GenericResponse(success=False,
+                               message="Please ensure that you have entered a valid public Instagram account username.",
+                               button_text="Retry", header="Error")
+
+    # Followers count
+    followers_count = user_data.get("edge_followed_by", {}).get("count", 0)
+
+    if followers_count == 0:
+        return GenericResponse(
+            success=False,
+            message="We encountered an issue while fetching data from Instagram. Please try again later.",
+            button_text="Retry",
+            header="Oops"
+        )
+
+    # Get recent posts (You might have to adjust this depending on available post data)
+    posts = user_data.get("edge_owner_to_timeline_media", {}).get("edges", [])
+
+    if not posts:
+        return GenericResponse(success=False,
+                               message="Please ensure that you have entered a valid public Instagram account username.",
+                               button_text="Retry", header="Error")
+
+    total_likes = 0
+    total_comments = 0
+    total_views = 0
+    post_count = 0
+
+    # Iterate through recent posts (Let's use the first 12 posts as an example)
+    for post in posts[:12]:  # Adjust this to the number of posts you want
+        node = post.get("node", {})
+        total_likes += node.get("edge_media_preview_like", {}).get("count", 0)
+        total_comments += node.get("edge_media_to_comment", {}).get("count", 0)
+        total_views += node.get("video_view_count", 0)  # For video posts, get the view count
+        post_count += 1
+
+    if post_count == 0:
+        return GenericResponse(
+            success=False,
+            message="We encountered an issue while fetching data from Instagram. Please try again later.",
+            button_text="Retry",
+            header="Oops"
+        )
+
+    # Calculate engagement rate
+    total_engagement = total_likes + total_comments + total_views
+    engagement_rate = (total_engagement / (followers_count * post_count))
+
+    return EnagementMetric(
+        engagement_rate=f"{engagement_rate:.1f}",
+        engagement_quality=engagement_rate_to_quality(engagement_rate),
+        followers=int_to_str_k(followers_count),
+        likes=int_to_str_k(int(total_likes / post_count)),
+        comments=int_to_str_k(int(total_comments / post_count))
+    )
+
+
+def calculate_engagement_rate1(username: str) -> EnagementMetric | GenericResponse:
+    L = instaloader.Instaloader()
+
+    try:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future = executor.submit(load_data_using_instaloader, L, username)
+            result = future.result(timeout=30)
+            return result
+
+    except concurrent.futures.TimeoutError:
+        return GenericResponse(
+            success=False,
+            message="The request timed out. Please try again later.",
+            button_text="Retry",
+            header="Oops"
+        )
+    except Exception as e:
+        return GenericResponse(
+            success=False,
+            message="We encountered an issue while fetching data from Instagram. Please try again later.",
+            button_text="Retry",
+            header="Oops"
+        )
+
+
+def load_data_using_instaloader(L, username: str) -> EnagementMetric | GenericResponse:
     try:
 
         profile = instaloader.Profile.from_username(L.context, username)
@@ -150,8 +302,8 @@ def load_profile_data(L, username: str) -> EnagementMetric | GenericResponse:
                                    message="No posts found",
                                    button_text="Understood", header="Oops")
 
-        avg_likes = total_likes / post_count
-        avg_comments = total_comments / post_count
+        avg_likes = total_likes
+        avg_comments = total_comments
         total_engagements = total_likes + total_comments + total_views
         engagement_rate = (total_engagements / (follower_count * post_count))
 
